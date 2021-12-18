f0776255bc5d900a120ec8e8a29b3191a5341a03
Igor Enrico <igorefp@gmail.com>
2021-12-18 17:05:14 -0300

    Update ex003.py

--- Curso Python/ex003.py
+++ Curso Python/ex003.py
@@ -1,3 +1,4 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
-print(f'A soma entre {n1} e {n2} é igual a {n1 + n2}!')
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} é igual a {s}!')
