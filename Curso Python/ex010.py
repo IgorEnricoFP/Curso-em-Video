a8db96fdd3cd17b8135e4e58d75d2727859fa946
Igor Enrico <igorefp@gmail.com>
2021-12-18 18:47:57 -0300

    Create ex010.py

---
+++ Curso Python/ex010.py
@@ -0,0 +1,4 @@
+c = float(input("Quanto dinheiro você tem na carteira? R$"))
+print(f"Com R${c:.2f} você pode comprar US${c / 5.70:.2f}")
+print(f"Com R${c:.2f} você pode comprar EUR${c / 6.40:.2f}")
+print(f"Com R${c:.2f} você pode comprar JPY${c * 19.96:.2f}")
