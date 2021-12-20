a62a515c24eeb9ffaa7005da23c8d5e15a6b6adf
Igor Enrico <igorefp@gmail.com>
2021-12-20 10:54:15 -0300

    Create aula008a.py

---
+++ Curso Python/aula008a.py
@@ -0,0 +1,4 @@
+from math import sqrt, floor, ceil
+n = int(input("Digite um número: "))
+r = sqrt(n)            # floor() = arredondar para baixo, ceil() = arredondar para cima
+print(f"A raiz quadrada de {n} é igual a {r:.2f}")
