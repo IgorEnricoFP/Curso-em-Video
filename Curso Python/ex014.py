d42b8ced2988cd035b7f778089fe11e10af0d394
Igor Enrico <igorefp@gmail.com>
2021-12-20 10:10:11 -0300

    Update ex014

--- Curso Python/ex014.py
+++ Curso Python/ex014.py
@@ -1,3 +1,4 @@
-celsius = int(input("Informe a temperatura em ºC: "))
-
-print(f"")
+c = float(input("Informe a temperatura em ºC: "))
+f = (c * 9/5) + 32
+k = c + 273.15
+print(f"A temperatura de {c}ºC corresponde a {f}ºF! e  a {k}K")
