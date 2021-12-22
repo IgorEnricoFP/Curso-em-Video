a7baf1e581fe26fade530a34d18436c021a3d6b7
Igor Enrico <igorefp@gmail.com>
2021-12-21 21:21:23 -0300

    Update ex019.py

--- Curso Python/ex019.py
+++ Curso Python/ex019.py
@@ -3,4 +3,4 @@ a1 = str(input("Escreva o nome do 1º Aluno: "))
 a2 = str(input("Escreva o nome do 2º Aluno: "))
 a3 = str(input("Escreva o nome do 3º Aluno: "))
 a4 = str(input("Escreva o nome do 4º Aluno: "))
-print(f"O aluno escolhido para apagar a lousa é "
+print(f"O aluno escolhido para apagar a lousa é ")
