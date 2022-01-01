6f82b8099eda699e8920fdf1dd3f0116488a0223
Igor Enrico <igorefp@gmail.com>
2021-12-31 21:12:35 -0300

    Update aula009a.py

--- Curso Python/aula009a.py
+++ Curso Python/aula009a.py
@@ -1,2 +1,15 @@
 frase = 'Curso em Video Python'
 print(f'{frase[9::3]}')
+
+frase = 'Curso em video Python'
+print(f"{len(frase)}")
+
+frase = 'Curso em video Python'
+print(f"{frase.count('o', 0 , 13)}")
+
+frase = 'Curso em video Python'
+print(f"{frase.find('deo')}")
+print(f"{frase.find('Android')}")
+
+frase = 'Curso em video Python'
+print(f"{'Curso' in frase}")
