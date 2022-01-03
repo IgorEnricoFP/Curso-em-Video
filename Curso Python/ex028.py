c3e9e77d8bf60f131e8f20938931ad9e2fc23220
Igor Enrico <igorefp@gmail.com>
2022-01-03 11:26:21 -0300

    Update ex028.py

--- Curso Python/ex028.py
+++ Curso Python/ex028.py
@@ -1,15 +1,15 @@
-import random
+from random import randint
 
-n = random.randint(0, 5)
-print("""-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-Vou pensar em número entre 0 e 5. Tente advinhar...
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=""")
+c = randint(0, 5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente advinhar...')
+print('-=-' * 20)
 u = int(input('Em que número eu pensei? '))
 print('Processando...')
 if u < 0 or u > 5:
     print('NÚMERO INVÁLIDO! Escolha um número de 0 a 5')
 else:
-    if n == u:
+    if c == u:
         print(f'PARABÉNS! Você conseguiu me vencer!')
     else:
-        print(f'GANHEI! Eu pensei no número {n} e não no {u}')
+        print(f'GANHEI! Eu pensei no número {c} e não no {u}')
