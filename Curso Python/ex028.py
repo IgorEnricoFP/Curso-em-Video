17e9012521b51452bb1400b5f56b33b048d9decc
Igor Enrico <igorefp@gmail.com>
2022-01-03 11:28:42 -0300

    Update ex028.py

--- Curso Python/ex028.py
+++ Curso Python/ex028.py
@@ -1,11 +1,12 @@
 from random import randint
-
+from time import sleep
 c = randint(0, 5)
 print('-=-' * 20)
 print('Vou pensar em um número entre 0 e 5. Tente advinhar...')
 print('-=-' * 20)
 u = int(input('Em que número eu pensei? '))
-print('Processando...')
+print('PROCESSANDO...')
+sleep(2)
 if u < 0 or u > 5:
     print('NÚMERO INVÁLIDO! Escolha um número de 0 a 5')
 else:
