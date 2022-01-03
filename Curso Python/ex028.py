f9b920b6ddf71fe3bbd610243d1b4496a3c58390
Igor Enrico <igorefp@gmail.com>
2022-01-03 13:07:45 -0300

    Update ex028.py

--- Curso Python/ex028.py
+++ Curso Python/ex028.py
@@ -2,10 +2,10 @@ from random import randint
 from time import sleep
 c = randint(0, 5)
 print("\033[33m-=-\033[m" * 20)
-print('\033[mVou pensar em um número entre 0 e 5. Tente advinhar...\033[m')
+print('\033[35mVou pensar em um número entre 0 e 5. Tente advinhar...\033[m')
 print("\033[33m-=-\033[m" * 20)
 u = int(input('Em que número eu pensei? '))
-print('\033[mPROCESSANDO...')
+print('\033[35mPROCESSANDO...')
 sleep(2)
 if u < 0 or u > 5:
     print('\033[31mNÚMERO INVÁLIDO!\033[m Escolha um número de 0 a 5')
@@ -13,4 +13,4 @@ else:
     if c == u:
         print(f'\033[32mPARABÉNS! Você conseguiu me vencer!')
     else:
-        print(f'GANHEI! Eu pensei no número {c} e não no {u}')
+        print(f'\033[31mGANHEI! Eu pensei no número {c} e não no {u}')
