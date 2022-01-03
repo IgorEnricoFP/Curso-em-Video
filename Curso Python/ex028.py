235432154bbb3ce3204fde6875118521b3947d01
Igor Enrico <igorefp@gmail.com>
2022-01-03 11:16:53 -0300

    Update ex028.py

--- Curso Python/ex028.py
+++ Curso Python/ex028.py
@@ -1,8 +1,10 @@
 import random
 
 n = random.randint(0, 5)
-print('-------Jogo de Advinhação-------')
-print("""Nesse jogo, você ganha se escolher o mesmo número que o computador \nVamos Começar!""")
+print("""-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= \n
+Vou pensar em número entre 0 e 5. Tente advinhar...\n
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=""")
+u = int(input('Em que número eu pensei? '))
 u = int(input('Escolha um número de 0 a 5: '))
 if u < 0 or u > 5:
     print('NÚMERO INVÁLIDO! Escolha um número de 0 a 5')
