c61d5f53e4a3f67321f62ce8a880c959a1386265
Igor Enrico <igorefp@gmail.com>
2022-01-03 10:35:09 -0300

    Update ex027.py

--- Curso Python/ex027.py
+++ Curso Python/ex027.py
@@ -1,5 +1,5 @@
-n = str(input('Digite seu nome completo: ')).strip()
+n = str(input('Digite seu nome completo: ')).strip().title()
 nome = n.split()
 print('Muito prazer em te conhecer!')
-print(f'O seu primeiro nome é {nome[0]}')
-print(f'O seu último nome é {nome}')
+print(f'Seu primeiro nome é {nome[0]}')
+print(f'Seu último nome é {nome[len(nome)-1]}')
