4eb046275c51bca51a1051ac95a482a01bf90a89
Igor Enrico <igorefp@gmail.com>
2022-01-03 11:22:42 -0300

    Update ex028.py

--- Curso Python/ex028.py
+++ Curso Python/ex028.py
@@ -1,16 +1,15 @@
 import random
 
 n = random.randint(0, 5)
-print("""-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= \n
-Vou pensar em número entre 0 e 5. Tente advinhar...\n
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=""")
+print("""-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+Vou pensar em número entre 0 e 5. Tente advinhar...
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=""")
 u = int(input('Em que número eu pensei? '))
-u = int(input('Escolha um número de 0 a 5: '))
+print('Processando...')
 if u < 0 or u > 5:
     print('NÚMERO INVÁLIDO! Escolha um número de 0 a 5')
 else:
-    print(f'O computador escolheu: {n}')
     if n == u:
-        print('Você Venceu!')
+        print(f'PARABÉNS! Você conseguiu me vencer!')
     else:
-        print('Você Perdeu!')
+        print(f'GANHEI! Eu pensei no número {n} e não no {u}')
