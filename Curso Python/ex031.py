9191424c84072a0c453a88a31dee20f0a650fee1
Igor Enrico <igorefp@gmail.com>
2022-01-03 14:13:50 -0300

    Update ex031.py

--- Curso Python/ex031.py
+++ Curso Python/ex031.py
@@ -1,2 +1,4 @@
-d = float(input('Qual a distância da sua viagem? '))
-print(f'Você está prestes a começar uma viagem de {d:.2f}Km.')
+dis = float(input('Qual a distância da sua viagem? '))
+print(f'Você está prestes a começar uma viagem de {dis:.2f}Km.')
+preco = dis * 0.50 if dis <= 200 else dis * 0.45
+print(f'E o preço da sua viagem será de R${preco:.2f}')
