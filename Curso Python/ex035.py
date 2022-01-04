72ef5115cea882b8a5e891e4a30983072ffb914d
Igor Enrico <igorefp@gmail.com>
2022-01-04 15:05:08 -0300

    Create ex035.py

---
+++ Curso Python/ex035.py
@@ -0,0 +1,9 @@
+cores = {"normal": '\033[m',
+         "amarelo": '\033[33m',
+         "roxo": '\033[35m'}
+print(f'{cores["amarelo"]}-=-' * 20)
+print(f'{cores["roxo"]}Analisador de Triângulos')
+print(f'{cores["amarelo"]}-=-' * 20)
+s1 = float(input('Primeiro segmento: '))
+s2 = float(input('Segundo segmento: '))
+s3 = float(input('Terceiro segmento: '))
