8939178f4c66601a621dba8154055cb11e6b155f
Igor Enrico <igorefp@gmail.com>
2022-01-04 14:41:20 -0300

    Update ex033.py

--- Curso Python/ex033.py
+++ Curso Python/ex033.py
@@ -1,4 +1,15 @@
 n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
 n3 = int(input('Terceiro valor: '))
-valores = {n1, n2, n3}
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+else:
+    maior = n3
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
