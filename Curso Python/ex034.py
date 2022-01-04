970b7084976371b5188500698207beaf09497795
Igor Enrico <igorefp@gmail.com>
2022-01-04 14:56:56 -0300

    Update ex034.py

--- Curso Python/ex034.py
+++ Curso Python/ex034.py
@@ -1,4 +1,10 @@
 cores = {"azul": '\033[36m',
-         "verde": '\033[34m'}
-sal1 = float(input(''))
-print(f'{cores["azul"]}{sal1}{cores["verde"]}{sal2}')
+         "vermelho": '\033[31m',
+         "normal": '\033[m'}
+sal1 = float(input('Qual é o salário do funcionário? R$'))
+sal2 = sal1 * (10 / 100)
+if sal1 <= 1250:
+    sal2 = sal1 + (sal1 * 15 / 100)
+else:
+    sal2 = sal1 + (sal1 * 10 / 100)
+print(f'Quem ganhava {cores["vermelho"]}R${sal1:.2f} {cores["normal"]}passa a ganhar {cores["azul"]}R${sal2:.2f}')
