9558bae410895910ad1cdbae9ed624200f103352
Igor Enrico <igorefp@gmail.com>
2022-01-04 15:20:54 -0300

    Update aula011.py

--- Curso Python/aula011.py
+++ Curso Python/aula011.py
@@ -2,3 +2,6 @@ print('\033[0;33;44mFonte padrão texto amarelo e fundo azul')
 print('\033[1;31;47mFonte em negrito texto vermelho e fundo cinza')
 print('\033[4;32;40mFonte sublinhada texto verde fundo preto')
 print('\033[7;35;43mFonte Invertida texto roxo fundo amarelo')
+print('\033[mfonte normal')
+a = 19 // 2
+print(a)
