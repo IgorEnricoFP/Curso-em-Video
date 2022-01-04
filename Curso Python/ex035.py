bd74f891ac35ec7328619da059a9f9fea481d5fe
Igor Enrico <igorefp@gmail.com>
2022-01-04 15:13:40 -0300

    Update ex035.py

--- Curso Python/ex035.py
+++ Curso Python/ex035.py
@@ -7,3 +7,7 @@ print(f'{cores["amarelo"]}-=-' * 20)
 s1 = float(input('Primeiro segmento: '))
 s2 = float(input('Segundo segmento: '))
 s3 = float(input('Terceiro segmento: '))
+if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
+    print('Os segmentos acima PODEM FORMAR um triângulo')
+else:
+    print('Os segmentos NÃO PODEM FORMAR um triângulo')
